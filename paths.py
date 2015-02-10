e885f706d235c88e73e73706bf9f2f8305ce9a9b
saltire sable <x@saltiresable.com>
2015-02-10 18:28:19 -0500

    Update pathfinding functions
    This part is still untested, but should be nearly complete.

--- paths.py
+++ paths.py
@@ -3,95 +3,139 @@ class Cell:
         # dict indexed by destination coords,
         # containing a list of tuples with the distance to the destination along a certain route
         # and the coords of the next step on that route
-        self.dests = {}
+        self.routes = {}
+
+
+def can_enter(x, y):
+    return x >= 0 and y >= 0 and x < width and y < height
+
+
+def weight_direction(delta):
+    base = 1  # base weight for all directions
+    fwt = 25  # flat added weight for desired directions
+    pwt = 1   # proportional added weight for desired directions
+
+    return base + ((fwt + pwt * delta) if delta > 0 else 0)
+
+
+def next_step(prev, current, dest):
+    x, y = current
+    dx, dy = dest
+
+    # next cell to the N/E/S/W
+    ncells = [(x, y - 1), (x + 1, y), (x, y + 1), (x - 1, y)]
+    # distance to travel N/E/S/W
+    deltas = [y - dy, dx - x, dy - y, x - dx]
+
+    # calculate weights for each direction and make a weighted list
+    choices = []
+    for ndir in range(4):
+        ncell = ncells[ndir]
+
+        # skip this direction if we can't enter or it leads backward
+        if not can_enter(*ncell) or ncell == prev:
+            continue
+
+        # add to the choices list a weighted number of times
+        choices += [ncell] * weight_direction(deltas[ndir])
+
+    # pick a direction from the weighted list and get the next cell
+    return random.choice(choices)
+
+
+# pick steps toward a destination cell, returning when we hit an existing cell
+def find_path(cells, start, dest):
+    path = []
+    prev = None, None
+    current = start
+    while True:
+        nextcell = next_step(prev, current, dest)
+        path.append(nextcell)
+        if isinstance(cells.get(nextcell), Cell):
+            return path
+
+        prev = current
+        current = nextcell
+
+
+# add a set of new routes to a cell, and propagate them along that cell's existing routes
+def propagate_routes_to(cells, prev, current, routes):
+    new_routes = {}
+
+    for dest, (rdist, _) in routes.items():
+        rdist += 1
+        existing_dist, _ = cells[current].routes.get(dest, (None, None))
+
+        # figure out which of the new routes are in fact new or shorter
+        # then save them and keep track of whether we made changes
+        if existing_dist is None or rdist < existing_dist:
+            cells[current].routes[dest] = rdist, prev
+            new_routes[dest] = cells[current].routes[dest]
+
+    # propagate the new routes along all the unchanged routes
+    for dest, (_, nextstep) in cells[current].routes.items():
+        if dest not in new_routes:
+            propagate_routes_to(cells, current, nextstep, new_routes)
 
 
 # follow a list of coordinates, marking the distance back to the origin at each step
-def blaze_path(cells, start, path):
-    origin_dists = get_origin_distances(cells, start)
-    px, py = start
+def blaze_path(cells, prev, path):
+    start_routes = cells[prev].routes
     distance = 0
-    for x, y in path:
-        # create a cell at the next step in the path
-        cell = cells.setdefault((x, y), Cell())
 
-        # set the previous step and cumulative distance to all the origin points we are tracking
+    for current in path:
         distance += 1
-        for origin, odist in origin_dists:
-            cell.dests.setdefault(origin, set()).add({'step': (px, py),
-                                                      'distance': odist + distance})
-        px, py = x, y
+
+        # create a cell at every step except the last one, which should already be a cell
+        if current != path[-1]:
+            cells[current] = Cell()
+            # continue each of the origin cell's routes in this cell
+            for dest, (rdist, _) in start_routes.items():
+                cells[current].routes[dest] = rdist + distance, prev
+
+            prev = current
+
+    # now do the last cell, and propagate routes across the new connection
+    new_routes = {}
+    # we will be sending routes from this cell back along any new/shorter routes
+    back_routes = {current: 0, None}
+
+    # figure out which of the new routes are in fact new or shorter
+    for dest, (rdist, _) in start_routes.items():
+        rdist += distance
+        existing_dist, _ = cells[current].routes.get(dest, (None, None))
+
+        # figure out which of the new routes are in fact new or shorter
+        # then save them and keep track of whether we made changes or not
+        if existing_dist is None or rdist < existing_dist:
+            cells[current].routes[dest] = rdist, prev
+            new_routes[dest] = cells[current].routes[dest]
+        else:
+            back_routes[dest] = cells[current].routes[dest]
+
+    # propagate the new routes along all the unchanged routes, and vice versa
+    for dest, (_, nextstep) in cells[current].routes.items():
+        propagate_routes_to(cells, current, nextstep,
+                            back_routes if dest in new_routes else new_routes)
 
 
-# follow the shortest path from the start to the destination,
-# marking the distances back to a set of origin points at each step
-def follow_path(cells, start, dest):
-    origin_dists = get_origin_distances(cells, start)
-    px, py = start
-    distance = 0
-    while True:
-        # find the next step on the shortest path to the destination
-        _, x, y = sorted(cells[px, py].dests[dest])[0]
-        cell = cells[x, y]
-
-        # set the previous step and cumulative distance to all the origin points we are tracking
-        distance += 1
-        for origin, odist in origin_dists:
-            cell.dests.setdefault(origin, set()).add({'step': (px, py),
-                                                      'distance': odist + distance})
-        px, py = x, y
-
-
-def get_shortest_route(routes):
-    return sorted(routes, lambda x, y: cmp(x.distance, y.distance))[0]
-
-
-def get_origin_distances(cells, origin):
-    # get the coordinates and shortest distance to each city connected to the origin
-    origin_dists = set((dest, get_shortest_route(routes).distance)
-                       for dest, routes in cells[origin].dests.items())
-    # including the origin itself
-    origin_dists.add((origin, 0))
-    return origin_dists
-
-
-def link_cities(origin, dest):
+def link_cities(cells, origin, dest):
     current = origin
     distance = 0
-    while True:
-        path = get_path(origin, current, dest)
-        newcell = path[-1]
+    while current != dest:
+        # find the next leg of the path from the current cell to the destination
+        start = current
+        path = find_path(start, dest)
+        current = path[-1]
 
-        if origin in cells[newcell].dests:
+        if origin in cells[current].dests:
             # there is already a path from origin to here - don't blaze a new one
-            # even if it's longer than the current path
-            current = newcell
-            distance = get_shortest_route(cells[newcell].dests[origin]).distance
+            # even if the existing one is longer
+            distance, _ = cells[current].routes[origin]
             continue
 
         # mark the route from the current coord to the end of the path
-        blaze_path(cells, current, path)
+        blaze_path(cells, start, path)
 
-        # TODO: change routes so it holds a single shortest route instead of a set
-        routes = cells[newcell].routes
-        routes[newcell] = 0, newcell
-        for distance, (x, y) in cells[newcell].routes.values():
-            propagate_to(cells[x, y], routes)
-
-
-def propagate_to(cell, routes):
-    # figure out which of the new routes are in fact new or shorter
-    added_routes = {}
-    for (rx, ry), (distance, (x, y)) in routes:
-        distance += 1
-        existing_dist, _ = cell.routes.get((rx, ry), (None, None))
-        if existing_dist is None or distance < existing_dist:
-            added_routes[rx, ry] = distance, (x, y)
-
-    # update this cell's routes with the new/shorter ones
-    cell.routes.update(added_routes)
-
-    # propagate the new/shorter routes along all the other routes
-    for rx, ry in cell.routes:
-        if rx, ry not in added_routes:
-            propagate_to(cells[rx, ry], added_routes)
+        if current == dest:
+            break
