8b52dc37c44e809106f7a9ee8a9257c0a9b78fd7
saltire sable <x@saltiresable.com>
2015-03-04 19:54:31 -0500

    Slightly more compact function

--- cities.py
+++ cities.py
@@ -4,17 +4,12 @@ import random
 def place_cities(width, height, citycount, distance):
     cities = []
 
-    def city_isolated(cx, cy, cities, distance):
-        for nx, ny in cities:
-            if abs(nx - cx) < distance or abs(ny - cy) < distance:
-                return False
-        return True
-
     while len(cities) < citycount:
         cx = random.randint(0, width - 1)
         cy = random.randint(0, height - 1)
 
-        if not city_isolated(cx, cy, cities, distance):
+        if any((abs(nx - cx) < distance or abs(ny - cy) < distance)
+               for nx, ny in cities):
             continue
 
         cities.append((cx, cy))
